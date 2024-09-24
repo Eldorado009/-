5369ae52606ddc88a99deed71ea3b1a35a995c7a
Eldorado009 <eldarna@code.edu.az>
2024-09-24 23:10:08 +0400

    first commit

---
+++ SQLQuery4.sql
@@ -0,0 +1,4 @@
+--SELECT TOP (1000) [Id]
+--      ,[Name]
+--      ,[CreatedDate]
+--  FROM [EFProjectDb].[dbo].[Categories]
---
+++ SQLQuery6.sql
@@ -0,0 +1,7 @@
+--SELECT TOP (1000) [Id]
+--      ,[FullName]
+--      ,[UserName]
+--      ,[Email]
+--      ,[Password]
+--      ,[CreatedDate]
+--  FROM [EFProjectDb].[dbo].[Users]
